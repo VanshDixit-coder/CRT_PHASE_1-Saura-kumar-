07c5166ba7c706b40ddcee0dd2322163a3d6b6e8
VanshDixit-coder <fakevansh2004@gmail.com>
2026-05-02 11:38:38 +0530

    Normal commit

---
+++ Day-2/second.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("Sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}
