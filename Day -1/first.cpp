a33f32b14e9a5db1cd79e0b7819b876bdb33cf7c
VanshDixit-coder <fakevansh2004@gmail.com>
2026-05-06 23:46:24 +0530

    CRT day 1

--- Day -1/first.cpp
+++ Day -1/first.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    cout<<"Hello World";
+    int r,c;
+    cin>>r>>c;
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
     return 0;
-}
+} 
