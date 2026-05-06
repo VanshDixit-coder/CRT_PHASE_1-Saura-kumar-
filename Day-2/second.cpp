9851345ee1890d3c9cc5ee620e4e89f81f680c4f
VanshDixit-coder <fakevansh2004@gmail.com>
2026-05-07 00:10:27 +0530

    CRT day-2

--- Day-2/second.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-using namespace std;
-int main(){
-    int a,b;
-    printf("Enter two numbers: ");
-    scanf("%d %d",&a,&b);
-    printf("Sum of %d and %d is %d",a,b,a+b);
-    return 0;
-}
